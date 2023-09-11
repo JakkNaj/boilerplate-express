f8a4f43576b22a3ab437a2b36f751781e2b04ca3
Jakub Najman <jakub@najman.cz>
2023-09-11 21:10:20 +0200

    .env message style usage

--- myApp.js
+++ myApp.js
@@ -1,6 +1,24 @@
+let dotenv = require('dotenv').config(); //loads the environment variables
 let express = require('express');
 let app = express();
+app.use("/public" ,express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+ let absolutePath = __dirname + '/views/index.html';
+ res.sendFile(absolutePath);
+});
+
+app.get("/json", (req, res) => {
+ if (process.env.MESSAGE_STYLE === "uppercase") {
+  res.json({
+   message: "HELLO JSON"
+  });
+ } else {
+  res.json({
+   message: "Hello json"
+  });
+ }
+})
 
 
 
