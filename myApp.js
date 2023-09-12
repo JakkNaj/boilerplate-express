454694b60add8910ff4df95ee7b69befeb794f66
JakkNaj <kubanaj@gmail.com>
2023-09-12 13:27:19 +0000

    get input from client exercises done

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
 let dotenv = require('dotenv').config(); //loads the environment variables
+let bodyParser = require('body-parser')
 let express = require('express');
 let app = express();
-app.use("/public" ,express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+app.use(function (req, res, next) {
+ let string = req.method + " " + req.path + " - " + req.ip;
+ console.log(string);
+ next();
+});
+
+app.use( bodyParser.urlencoded({extended: false}) )
 
 app.get("/", (req, res) => {
  let absolutePath = __dirname + '/views/index.html';
@@ -20,16 +28,34 @@ app.get("/json", (req, res) => {
  }
 })
 
+app.get("/now", function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({
+    "time": req.time
+  });
+});
 
+app.get("/:word/echo", (req, res) => {
+  res.json({
+    "echo": req.params.word
+  })
+})
 
-
-
-
-
-
-
-
-
+app.route("/name").get((req, res) => {
+  let firstname = req.query.first;
+  let lastname = req.query.last;
+  res.json({
+    "name": `${firstname} ${lastname}`
+  })
+}).post((req,res) => {
+  let firstname = req.body.first;
+  let lastname = req.body.last;
+  res.json({
+    "name": `${firstname} ${lastname}`
+  })
+});
 
 
 
